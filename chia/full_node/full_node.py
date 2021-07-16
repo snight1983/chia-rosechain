70787ddf6f3734233dda39e7ed008f490ddaece4
zhantao <zhan.tao@cyclone-robotics.com>
2021-07-16 16:15:09 +0800

    dns

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -187,7 +187,7 @@ class FullNode:
             dns_servers = self.config["dns_servers"]
         elif self.config["port"] == 8520:
             # If `dns_servers` misses from the `config`, hardcode it if we're running mainnet.
-            dns_servers.append("dns-introducer.chia.net")
+            dns_servers.append("dns-introducer.chiarose.com")
         try:
             self.full_node_peers = FullNodePeers(
                 self.server,
